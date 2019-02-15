4fe1b018bb90101ba4e8f28cd7d5dc441ad45f69
Ertheo Siswadi <esiswadi@g.ucla.edu>
2019-02-14 23:49:07 -0800

    refactored make_tokens. separated into multiple files for organization and modularity

--- handler.js
+++ handler.js
@@ -1,125 +1,3 @@
-var input_stack = [];
-var finish = false;
-
-let syntax_error = {
-	name: 'SYNTAX ERROR',
-	message:"♥ SYNTAX ERROR ♥"
-}
-
-let math_error = {
-	name: 'MATH ERROR',
-	message:"◈ MATH ERROR ◈"
-}
-
-let over_error = {
-	name: 'OUT OF BOUNDS ERROR',
-	message:"◈ MATH ERROR ◈"
-}
-
-$(document).ready(() =>{
-	$('.keypad_n').click((e)=>{
-		if(finish)
-		{
-			input_stack.length = 0;
-			finish = false;
-		}
-		var key_id = $(e.target)[0].id
-		var input = $('#'+key_id).html();
-
-		input_stack.push(input);
-		refresh_display();
-	});
-
-	$('.keypad_op').click((e)=> {
-		finish = false;
-		var key_id = $(e.target)[0].id
-		var input = $('#'+key_id).html();
-
-		input_stack.push(input);
-		refresh_display();
-	})
-
-	$('.keypad_trig').click((e)=> {
-		if(finish)
-		{
-			input_stack.length = 0;
-			finish = false;
-		}
-		var key_id = $(e.target)[0].id
-		var input = $('#'+key_id).html();
-
-		input_stack.push(input);
-		input_stack.push('(');
-		refresh_display();
-	});
-
-	$('#backspace').click(()=>{
-		input_stack.pop();
-		refresh_display();
-	});
-
-	$('#AC').click(()=>{
-		input_stack.length = 0;
-		refresh_display();
-	});
-
-	$('#equal').click(() => {
-		if(input_stack.length != 0)
-		{
-			var result = '';
-			try
-			{
-				result = solve(make_tokens(input_stack.join("")));
-				if(isNaN(result))
-				{
-					console.log(result);
-					throw syntax_error; 
-				}
-				else if(result == Infinity)
-				{
-					throw math_error; 
-				}
-				result = result.toString();
-				//check if result is too long and cannot be displayed
-				if(result.length > 17 && result.includes('.')) //refactor and make a global constant
-				{
-					result = result.slice(0,17);
-				}
-				$('#math').prop('readonly', false);
-				$('#math').val(result);
-				$('#math').prop('readonly', true);
-
-				input_stack.length = 0;
-				input_stack.push(result);
-
-				finish = true;
-			}
-			catch(err)
-			{
-				console.error(err.name);
-				if(err.name == 'RangeError')
-				{
-					err.message = '♥ SYNTAX ERROR ♥'
-				}
-				$('#math').prop('readonly', false);
-				$('#math').val(err.message);
-				$('#math').prop('readonly', true);
-
-				input_stack.length = 0;
-				finish = true;
-			}
-		}
-	});
-
-});
-
-function refresh_display()
-{
-	$('#math').prop('readonly', false);
-	$('#math').val(input_stack.join(""));
-	$('#math').prop('readonly', true);
-}
-
 function make_tokens(input)//input is a string and not an array, careful
 {
 	var collect = false;
@@ -129,106 +7,59 @@ function make_tokens(input)//input is a string and not an array, careful
 	var layer = 0;
 
 	var e;
-	// Array.from(input).forEach((e) =>
+	
 	for(i = 0; i < input.length; i++)
 	 {
-	 	e = input[i];
+	 	e = get_operator(input[i]);
+
+	 	layer = update_layer(e, layer);
+	 	n_period = update_period(e, n_period);
+
 		console.log('E is: ', e);
-		if(!isWhitespace(e))
+
+		let is_multiple_char_token = (isNumber(e) || isPeriod(e) || isAlphabet(e));
+		let start_create_token = (!isWhitespace(e) && !collect && is_multiple_char_token);
+		let push_single_char_token = (!isWhitespace(e) && !collect && !is_multiple_char_token);
+		let add_to_token = (!isWhitespace(e) && collect && is_multiple_char_token);
+		let stop_create_token = (!isWhitespace(e) && collect && !is_multiple_char_token);
+
+		let op_after_op = (isOperator(e) && toReturn.length > 0 && isOperator(toReturn[toReturn.length -1]));
+		let dot_after_dot = (n_period > 1);
+		let negative_layer = (layer < 0);
+
+		if(op_after_op || negative_layer || dot_after_dot)
+			throw syntax_error;
+
+		if(start_create_token)
 		{
-			if(!collect)
-			{
-				if(isNumber(e) || isPeriod(e) || isAlphabet(e))
-				{
-					if(isPeriod(e))//refactor
-					{
-						n_period++;
-					}
-					if(n_period > 1)//too many periods
-					{
-						throw syntax_error;
-					}
+			collect = true;
+			token = e;
+		}
+		else if(add_to_token)
+		{
+			token += e;	
+		}
+		else if(stop_create_token)
+		{
+			collect = false;
+			toReturn.push(token);
 
-					collect = true;
-					token = e;
-				}
-				else
-				{
-					e = ifOperator(e);
-
-					if((layer = updateLayer(e, layer)) < 0)
-					{
-						throw syntax_error;
-					}
-					// console.log(isOperator(e), 'e: ', e, 'toReturn: ', toReturn);
-					
-					console.log('length toRet: ',toReturn.length)
-					if(toReturn.length > 0)
-					{
-						
-						if(isOperator(e) && isOperator(toReturn[toReturn.length -1]))
-						{
-							throw syntax_error;
-						}
-					}
-					
-					token = e;
-					toReturn.push(e);
-				}
-			}
-			else
-			{
-				if(isNumber(e) || isPeriod(e) || isAlphabet(e))
-				{
-					if(isPeriod(e))
-					{
-						n_period++;
-					}
-					if(n_period > 1)//too many periods
-					{
-						throw syntax_error;
-					}
-
-					token += e;
-				}
-				else
-				{
-					e = ifOperator(e);
-					collect = false;
-					toReturn.push(token);
-
-
-					if((layer = updateLayer(e, layer)) < 0)
-					{
-						throw syntax_error;
-					}
-					if(toReturn.length > 0)
-					{
-						if(isOperator(e) && isOperator(toReturn[toReturn.length - 1]))
-						{
-							throw syntax_error;
-						}
-					}
-
-
-					n_period = 0;
-					token = e;
-					toReturn.push(token);
-				}
-			}
-			// console.log('token: ', token, ' collect: ', collect, ' e: ', e, ' toReturn: ', toReturn);
-		}	
+			n_period = 0;
+			token = e;
+			toReturn.push(token);	
+		}
+		else if(push_single_char_token)
+		{
+			token = e;
+			toReturn.push(e);			
+		}
 	}
-	// );
-	console.log('final toReturn:', toReturn.length);
 
 	if(collect)
 		toReturn.push(token);
 
 	if(layer != 0)
-	{
 		throw syntax_error;
-	}
 
 	console.log('tokens: ', toReturn);
 	return toReturn;
@@ -392,131 +223,4 @@ function solve_2(tokens)
 }
 
 
-function updateLayer(c, layer)
-{
-	if(isOpenParanthesis(c))
-	{
-		layer++;
-	}
-	else if(isCloseParanthesis(c))
-	{
-		layer--;
-	}
-	return layer;
-}
 
-//filter. if character is an operator, make sure it is the symbol we are using and not the one on display
-function ifOperator(c)
-{
-	var map_operator = {
-		'×':'*',
-		'÷':'/',
-		'−':'-',
-		'+':'+',
-		'*':'*',
-		'/':'/',
-		'-':'-',
-		'+':'+'
-	};
-	if(isOperator(c))
-	{
-		return map_operator[c];
-	}
-	else
-	{
-		return c;
-	}
-}
-function isOperator(c)
-{
-	let operators = /[×÷−+\-+*/]/;
-	if(c.match(operators) != null)
-	{	
-		return true;
-	}
-	return false;
-}
-
-function isOpenParanthesis(c)
-{
-	return (c == '(');
-}
-function isCloseParanthesis(c)
-{
-	return (c == ')');
-}
-
-function isWhitespace(c)
-{
-	let space = /[\s]/;
-	if(c.match(space) != null)
-	{
-		return true;
-	}
-	return false;
-}
-
-function isAlphabet(c)
-{
-	let alphabet = /[a-zA-z]/;
-	if(c.match(alphabet) != null)
-	{
-		return true;
-	}
-	return false;
-}
-
-function isNumber(c)
-{
-	let number = /[0-9]/;
-	if(c.match(number) != null)
-	{
-		return true;
-	}
-	return false;
-}
-
-function isPeriod(c)
-{
-	let number = /[\.]/;
-	if(c.match(number) != null)
-	{
-		return true;
-	}
-	return false;
-}
-
-function calculate(op, a, b)
-{
-	var select = {
-		'*': mul(a, b),
-		'×': mul(a, b),
-		'x': mul(a, b),
-		'/': div(a, b),
-		'÷': div(a, b),
-		'+': add(a, b),
-		'-': sub(a, b),
-		'−': sub(a, b)
-	}
-	return select[op];
-}
-
-function add(a, b)
-{
-	return a + b;
-}
-
-function mul(a, b)
-{
-	return a * b;
-}
-
-function div(a, b)
-{
-	return a / b;
-}
-
-function sub(a, b)
-{
-	return a - b;
-}
